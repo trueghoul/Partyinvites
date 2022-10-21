f339747e61baa594abd06c44a318cd1f8ec9c91f
trueghoul <vaneke@inbox.ru>
2022-10-22 01:40:46 +0400

    Add project files.

---
+++ Partyinvites/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Partyinvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Partyinvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Мorning" : "Good Afternoon";
+            return View("ResultIndex");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponses(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+
+
+        /*public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }*/
+    }
+}
---
+++ Partyinvites/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Partyinvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Введите ваше имя.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Введите ваш e-mail.")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Повторите попытку ввода e-mail адреса.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Введите ваш телефон.")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Укажите, пожалуйста, пойдете ли вы на вечеринку.")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
+
